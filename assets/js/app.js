fce66bee4ae08fd7d850bfa872fad3fe83b0aae4
Jason <55947509+Jasonndlovu@users.noreply.github.com>
2023-06-26 02:33:04 +0200

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+
+// smooth scroll
+$(document).ready(function(){
+	$(".nav-link").on('click', function(event) {
+
+    	if (this.hash !== "") {
+
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+      	} 
+    });
+});
+
+
+fetch('https://rest-api-cyyo.onrender.com/api/v1/products/get/featured/1')
+
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the request
+    console.error(error);
+  });
